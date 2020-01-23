9b9dff1b69be472d38157fad612477ffcc7057f2
Jorge Antônio da Silva <jorgeants@gmail.com>
2020-01-23 00:31:19 -0300

    Structure layout and style

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={event => setGithubUsername(event.target.value)}
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={event => setTechs(event.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={event => setLatitude(event.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={event => setLatitude(event.target.value)}
+              />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/1761659?s=460&v=4" alt="Jorge Silva"/>
+              <div className="user-info">
+                <strong>Jorge</strong>
+                <span>ReactJS, React Native</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam necessitatibus minus temporibus, expedita itaque.</p>
+            <a href="https://github.com">Acessar perfil no Github </a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
---
+++ front/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Header = (props) => (
+  <h1>{props.title}</h1>
+);
+
+export default Header;
